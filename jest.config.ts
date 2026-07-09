93060c35e9307a5f2a902f4696f646ade0a464d7
B-Chetan <168255688+B-Chetan@users.noreply.github.com>
2026-07-09 20:20:33 +0530

    update

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,8 @@ import type { Config } from "@jest/types";
 const config: Config.InitialOptions = {
   preset: "ts-jest",
   testEnvironment: "node",
+  moduleFileExtensions: ["ts", "tsx", "js"],
+  testPathIgnorePatterns: ["/node_modules/", "/.next/"],
 };
 
 export default config;
